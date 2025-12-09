f5a54206ac0fc85c5fd9a3845167947f5bb7a20f
teymuri-alireza <kishijung@gmail.com>
2025-12-09 14:42:43 +0330

    feat: add DateOnly column for task model

--- PlanCLI/Models/TodoItem.cs
+++ PlanCLI/Models/TodoItem.cs
@@ -5,5 +5,6 @@ public class TodoItem
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; } = "";
+    public DateOnly? Date { get; set; }
     public bool IsDone { get; set; } = false;
 }
